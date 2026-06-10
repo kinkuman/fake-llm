5b6a23c1e56e3567659680a75c6d61e25b7f0dba
kinkuman <kinkuman@gmail.com>
2026-06-10 11:24:03 +0900

    起動用コマンドラインシェル

---
+++ cli.sh
@@ -0,0 +1,4 @@
+uv run fake-llm chat --system-prompt 'pattern: ^ping$ => pong 
+pattern: ^ばか$ => バカって言うやつがバカなんだよ
+' 
+# --force-system-patterns
---
+++ run.sh
@@ -0,0 +1,2 @@
+uv run fake-llm serve --host 127.0.0.1 --port 8000 
+#--force-system-patterns
